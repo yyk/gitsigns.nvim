8190fb0286d398911c9c6e767aa8229f2b14fba0
Yuankai Yu <yuankaiyu@roblox.com>
2022-08-23 09:48:19 -0700

    fix some more

--- lua/gitsigns/current_line_blame.lua
+++ lua/gitsigns/current_line_blame.lua
@@ -39,6 +39,9 @@ end
 
 local reset = function(bufnr)
    bufnr = bufnr or current_buf()
+   if not api.nvim_buf_is_valid(bufnr) then
+    return
+   end
    api.nvim_buf_del_extmark(bufnr, namespace, 1)
    pcall(api.nvim_buf_del_var, bufnr, 'gitsigns_blame_line_dict')
 end
